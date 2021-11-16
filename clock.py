2fd4e50734a395e1418f6ed50668adc1a6053f7c
tycho <99062718@mydavinci.nl>
2021-11-16 16:25:05 +0100

    created clock.py

---
+++ clock.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import tkinter
+
+mainWindow = tkinter.Tk()
+mainWindow.title("De klok")
+mainWindow.configure(width=500, height=200, bg="white")
+currentText = tkinter.StringVar(value=0)
+
+def setTime():
+    currentTime = datetime.now()
+    currentText.set(currentTime.strftime("%H:%M:%S"))
+    mainWindow.after(200, setTime)
+
+label = tkinter.Label(mainWindow)
+label.configure(textvariable=currentText, font=("comic sans", 40))
+label.pack(ipadx = 500, ipady = 200)
+
+mainWindow.after(200, setTime)
+
+mainWindow.mainloop()
